4ab275f1e05b225d019085d9fd6a01fa0d6f9a17
prabhatadvait <prabhatsharma84226@gmail.com>
2025-09-07 08:46:47 +0530

    Simple sum(case)

---
+++ StrataScratch/63_titanic_survivors_and_non_survivors.sql
@@ -0,0 +1,7 @@
+SELECT 
+    survived,
+    SUM(CASE WHEN pclass = 1 THEN 1 ELSE 0 END) AS first_class,
+    SUM(CASE WHEN pclass = 2 THEN 1 ELSE 0 END) AS second_class,
+    SUM(CASE WHEN pclass = 3 THEN 1 ELSE 0 END) AS third_class
+FROM titanic
+GROUP BY survived;
